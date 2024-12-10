77464383d8ab2a250c8db9fce7a11e4e281692b8
Gustavo René <renegustavo42@gmail.com>
2024-12-10 08:25:24 -0300

    Add project files.

---
+++ EFCoreSqlServer/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using EFCoreSqlServer.Context;
+using EFCoreSqlServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreSqlServer.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public ProductsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProduct()
+    {
+        return Ok(await _context.Products.ToListAsync());
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        await _context.Products.AddAsync(product);
+        await _context.SaveChangesAsync();
+
+        return Ok(product);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Product>> UpdateProduct(int id, [FromBody] Product productFromJson)
+    {
+        if (ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (product == null) return NotFound();
+
+        product.Name = productFromJson.Name;
+        product.Description = productFromJson.Description;
+        product.Price = productFromJson.Price;
+
+        await _context.SaveChangesAsync();
+
+        return Ok(product);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Product>> DeleteProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+
+        if (product == null) return NotFound();
+
+        _context.Remove(product);
+        await _context.SaveChangesAsync();
+        return Ok(product);
+    }
+}
---
+++ EFCoreSqlServer/Program.cs
@@ -0,0 +1,20 @@
+using EFCoreSqlServer.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
